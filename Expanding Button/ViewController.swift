9f644275ff7de5cbd30a7e20d02859b445fd6514
Sohailbasha <shlbsh94@gmail.com>
2017-11-14 17:56:07 -0500

    added assets, and made button appear on view.

--- Expanding Button/ViewController.swift
+++ Expanding Button/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let centerButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 0, y: 0, width: 56, height: 56)
+        button.backgroundColor = UIColor.lightGray
+        button.setImage(#imageLiteral(resourceName: "add"), for: .normal)
+        button.layer.cornerRadius = 0.5 * button.layer.bounds.height
+//        button.clipsToBounds = true
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.centerButton.center = self.view.center
+        self.centerButton.addTarget(self, action: #selector(self.buttonTapped(_:)), for: .touchUpInside)
+        self.view.addSubview(centerButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc fileprivate func buttonTapped(_ sender: UIButton) {
+        print("button tapped!")
+//        makeButtonsAppear()
     }
+    
+    func makeButtonsAppear(sender: UIButton) {
+        for i in 0...4 {
+            let button = UIButton()
+            button.frame = CGRect(x: 50, y: 50, width: 56, height: 56)
+            button.backgroundColor = UIColor.purple
+            print("button: \(i)")
+            
+            switch i {
+            case 0:
+                button.setImage(#imageLiteral(resourceName: "snapchat"), for: .normal)
+                button.alpha = 1
+                self.view.addSubview(button)
 
+            case 1:
+                button.setImage(#imageLiteral(resourceName: "google-plus"), for: .normal)
+                button.alpha = 1
+                self.view.addSubview(button)
+            case 2:
+                button.setImage(#imageLiteral(resourceName: "twitter"), for: .normal)
+                button.alpha = 1
+                self.view.addSubview(button)
+            case 3:
+                button.setImage(#imageLiteral(resourceName: "telegram"), for: .normal)
+                button.alpha = 1
+                self.view.addSubview(button)
+            case 4:
+                button.setImage(#imageLiteral(resourceName: "soundcloud"), for: .normal)
+                self.view.addSubview(button)
+            default:
+                return
+            }
+        }
+    }
+    
 
+    
+    
 }
 
